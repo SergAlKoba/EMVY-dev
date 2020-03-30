a7b4212e7307ee2f69f0a8f425997e351872ed47
SergAlKoba <Serg.Al.Koba@gmail.com>
2020-03-30 23:03:36 +0300

    menu

--- assets/dist/js/script.js
+++ assets/dist/js/script.js
@@ -3,7 +3,13 @@ $(document).ready(function(){
     $('.phone').mask('+7 (000) 000 00 00', {placeholder: "+7 (___) ___ __ __"});
 
     $('.open-menu').on('click', function(){
-        $(this).toggleClass('active');
+        $('.menu').addClass('active');
+        console.log(1)
+    });
+
+    $('.colse-menu').on('click', function(){
+        $('.menu').removeClass('active');
+        console.log(0)
     });
 
 });
